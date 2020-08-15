50ec95301c7cef85a130fad32b80a747a7d30225
Heri Matondang <60765461+hemath03@users.noreply.github.com>
2020-08-15 10:06:10 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/uas_ajax_heri/';
-$config['site_url'] = 'http://localhost/uas_ajax_heri/';
+$config['base_url'] = 'http://localhost/uas_ajax_heri-master/';
+$config['site_url'] = 'http://localhost/uas_ajax_heri-master/';
 
 /*
 |--------------------------------------------------------------------------
